0bde7e1f65b2318c51512696ea141f2949322313
denis <9172885049@mail.ru>
2021-04-05 23:40:30 +0300

    Добавил MapView

--- TestTaskRCAI/MainViewController/Controller/MainViewController.swift
+++ TestTaskRCAI/MainViewController/Controller/MainViewController.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import MapKit
 
 class MainViewController: UIViewController {
 
+    
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
 
